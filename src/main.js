737e9dd34bc92367eee4a8402b1781f5eec25c9f
Sartorra <55885075+Sartorra@users.noreply.github.com>
2022-09-16 14:09:33 -0400

    Updated names

--- src/main.js
+++ src/main.js
@@ -4,7 +4,7 @@ import express from 'express'
 let certPath = "/var/www/site/"
 
 const app = express()
-const ServerWrapper = new eJsServerWrapper(app, `${certPath}servercert.pem`, `${certPath}rootcert.pem`);
+const ServerWrapper = new eJsServerWrapper(app, `${certPath}key.pem`, `${certPath}cert.pem`);
 const port = 443
 
 // Basic Hello World Example
