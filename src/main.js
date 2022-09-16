d24c74400f0bd44481bb8d30fdc48e2d38dfaeda
Sartorra <55885075+Sartorra@users.noreply.github.com>
2022-09-16 14:10:39 -0400

    Forgot to remove old listener

--- src/main.js
+++ src/main.js
@@ -5,16 +5,11 @@ let certPath = "/var/www/site/"
 
 const app = express()
 const ServerWrapper = new eJsServerWrapper(app, `${certPath}key.pem`, `${certPath}cert.pem`);
-const port = 443
 
 // Basic Hello World Example
 app.get('/', (req, res) => {
     res.send('Hello World!')
 })
 
-// Start listening on port 443
-app.listen(port, () => {
-    console.log(`Example app listening at http://localhost:${port}`)
-})
-
+// Connect the listeners
 ServerWrapper.ConnectListeners();
