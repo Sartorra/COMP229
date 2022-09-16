3e34b6a741c459b54d8874778740387ecd5c83ac
Sartorra <55885075+Sartorra@users.noreply.github.com>
2022-09-16 08:46:42 -0400

    Hello world example.

---
+++ src/main.js
@@ -0,0 +1,13 @@
+const express = require('express')
+const app = express()
+const port = 443
+
+// Basic Hello World Example
+app.get('/', (req, res) => {
+    res.send('Hello World!')
+})
+
+// Start listening on port 443
+app.listen(port, () => {
+    console.log(`Example app listening at http://localhost:${port}`)
+})
