28e30b58176eb137a75705f79375a0b85eab20b8
Sartorra <55885075+Sartorra@users.noreply.github.com>
2022-10-20 13:39:34 -0400

    fix

--- src/server.js
+++ src/server.js
@@ -7,12 +7,18 @@ const args = process.argv.slice(2);
 if (args.length === 0) {
     console.log('No certificate specified\nNot running in https mode');
 } else {
+    // Slice the two certificates apart.
+    const certificates = args.toString().split(', ');
+    console.log(certificates);
     const options = {
-        key: args[0],
-        cert: args[1]
+        key: certificates[0],
+        cert: certificates[1]
     };
     https.createServer(options, expressApp).listen(443);
     console.log('Running in https mode');
 }
 
 http.createServer(expressApp).listen(80);
+
+
+// docker run -d -p 80:80 -p 443:443 --name express-app styxies/express_app:latest `cat /var/key.pem`,`cat /var/cert.pem`
