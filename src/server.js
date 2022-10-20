18ab7d26cfd490dd607a0d84a14face72df887ea
Sartorra <55885075+Sartorra@users.noreply.github.com>
2022-10-20 14:01:46 -0400

    I was being dumb, I forgot about program "..."

--- src/server.js
+++ src/server.js
@@ -7,12 +7,9 @@ const args = process.argv.slice(2);
 if (args.length === 0) {
     console.log('No certificate specified\nNot running in https mode');
 } else {
-    // Slice the two certificates apart.
-    const certificates = args.toString().split('END PRIVATE KEY-----');
-    console.log(certificates);
     const options = {
-        key: certificates[0],
-        cert: certificates[1]
+        key: args[0],
+        cert: args[1]
     };
     https.createServer(options, expressApp).listen(443);
     console.log('Running in https mode');
